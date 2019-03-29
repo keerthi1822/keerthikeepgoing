d466320358ad74584dd1cf47a8c840685a57e855
keerthi <keerthi1822@gmail.com>
2019-03-29 12:26:56 +0100

    challenge in process

--- challenge/challenge.js
+++ challenge/challenge.js
@@ -15,23 +15,17 @@ let i = 0;
 setInterval(() => {
   const img = document.createElement("img");
   //img.style.marginTop = "-800px";
-  
 
   img.src = HYFImages[Math.floor(Math.random() * 9)];
   document.body.appendChild(img);
-  let style =  window.getComputedStyle(img);
+  /* let style =  window.getComputedStyle(img);
 
-console.log("Current marginTop: " + style.marginTop);
-
-
+console.log("Current marginTop: " + style.marginTop); */
   let margin = Math.floor(Math.random() * 900);
-  console.log(margin);
-
   img.style = `margin-left:${margin}px;transform : translateY(-1000px)`;
 
   setTimeout(() => {
     img.style = `margin-left:${margin}px;transform : translateY(1000px);`;
   }, 500);
   i++;
- 
 }, 3000);
