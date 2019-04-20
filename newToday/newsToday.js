c854ebf21cbe399d68bdbf09a8b62fafdb671d8e
keerthi <keerthi1822@gmail.com>
2019-04-20 18:16:01 +0200

    audio not working

--- newToday/newsToday.js
+++ newToday/newsToday.js
@@ -55,7 +55,7 @@ function translateImages() {
                 };
                 let confetti = new ConfettiGenerator(confettiSettings);
                 confetti.render();
-                let audio = new Audio("New-beginning.mp3");
+                let audio = new Audio("trimmelody.mp3");
       audio.play();
             }, 500);
 
